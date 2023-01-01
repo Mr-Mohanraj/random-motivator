c298cc0110d027bd0b2ca50794135bfc320eea63
Mohanraj <170602mark@gmail.com>
2023-01-01 19:06:19 +0530

    new file:   .gitignore 	modified:   loginsystem/models.py

--- loginsystem/models.py
+++ loginsystem/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50,unique=True,blank=False)
+    password = models.CharField(max_length=16,blank=False)
+    created_at = models.DateTimeField(blank=False,null=False,auto_now_add=True)
+    modify_at  = models.DateTimeField(blank=False, null=False, auto_now_add=True)
